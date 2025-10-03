17d06edaec88d8998ecf6e180c4b816a29d3bc14
laura853 <lauralopess2006@gmail.com>
2025-10-03 18:41:12 -0300

    feat: configurando a main com react-dom (esqueci de colocar o arquivo no commit anterior)

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </StrictMode>,
+    </BrowserRouter>
+  </React.StrictMode>,
 )
