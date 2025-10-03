b174c071e53fe69277e6eaf898a95f999a1f4c88
laura853 <lauralopess2006@gmail.com>
2025-10-03 18:34:53 -0300

    feat: configurando a Main e o App.tsx com React Router

--- access-control-cp/src/App.tsx
+++ access-control-cp/src/App.tsx
@@ -1,1 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./routes/Login";
+import Cadastro from "./routes/Cadastro";
 
+export default function App(){
+    return(
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+                <Route path="/Login" element={<Login/>}/>
+                <Route path="/Cadastro" element={<Cadastro/>}/>
+            </Routes>
+        </div>
+    )
+}
