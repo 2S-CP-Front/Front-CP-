d3ba55f14ad1770c86f026eb85653e94b10ddebb
vlonerickk <richard.rcket@gmail.com>
2025-10-05 14:47:00 -0300

    feat(api): Envio dos dados para API (POST)

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -4,49 +4,54 @@ import { Header } from "../../compenents/Header/Header"; // Ajuste o caminho con
 
 
 type Inputs = {
-  nome: string;
-  nomeUsuario: string;
-  email: string;
+    nome: string;
+    nomeUsuario: string;
+    email: string;
 };
 
 type User = {
-  id: number;
-  nome: string;
-  nomeUsuario: string;
-  email: string;
+    id: number;
+    nome: string;
+    nomeUsuario: string;
+    email: string;
 };
 
 function Cadastro() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-    reset,
-  } = useForm<Inputs>();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setError,
+        reset,
+    } = useForm<Inputs>();
 }
 const resp = await fetch("http://localhost:3000/usuarios");
-    const usuarios: User[] = await resp.json();
+const usuarios: User[] = await resp.json();
 
-    const existeUser = usuarios.some(
-      (u) => u.nomeUsuario.toLowerCase() === data.nomeUsuario
-    );
-    const existeEmail = usuarios.some(
-      (u) => u.email.toLowerCase() === data.email
-    );
+const existeUser = usuarios.some(
+    (u) => u.nomeUsuario.toLowerCase() === data.nomeUsuario
+);
+const existeEmail = usuarios.some(
+    (u) => u.email.toLowerCase() === data.email
+);
 
-    if (existeUser) {
-      setError("nomeUsuario", {
+if (existeUser) {
+    setError("nomeUsuario", {
         type: "duplicate",
         message: "Nome de usuário já em uso.",
-      });
-    }
+    });
+}
 
-    if (existeEmail) {
-      setError("email", {
+if (existeEmail) {
+    setError("email", {
         type: "duplicate",
         message: "E-mail já cadastrado.",
-      });
-    }
+    });
+}
 
-    if (existeUser || existeEmail) return;
+if (existeUser || existeEmail) return;
+const post = await fetch("http://localhost:3000/usuarios", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+});
