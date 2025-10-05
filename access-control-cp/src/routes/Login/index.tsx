d01952f443c524ffa77b3b768489843c98cc2162
MaiconDouglas-dev <maicon.timot8@gmail.com>
2025-10-05 09:51:07 -0300

    feat: criado o localStorage para fazer com que no Inspect da pagina, o nome e email de usuario digitado apareça. Obs(falta fazer com que o nome e emial de usuario mude na tela).

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -6,14 +6,32 @@ type Inputs = {
   email: string
 }
 
+type User = {
+  id: number
+  nome: string
+  nomeUsuario: string
+  email: string
+};
 
 function Login() {
-    const {
+  const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const users = await fetch("http://localhost:3000/usuarios");
+    const parsedUsers = await users.json();
+
+    const findUser = parsedUsers.find((user: User) =>{
+      return user.nomeUsuario === data.nomeUsuario && user.email === data.email;
+    });
+    console.log(findUser);
+    localStorage.setItem("name", findUser.nome);
+
+    window.location.reload();
+  }
 
 
   return (
@@ -22,39 +40,41 @@ function Login() {
       <Header />
       <section className="grid place-content-center h-screen ">
         <h1>Login</h1>
-        <form className="px-4 py-2 rounded-2xl shadow-lg grid gap-y-2 min-w-100" onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className="px-4 py-2 rounded-2xl shadow-lg grid gap-y-2 min-w-100"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="grid gap-y-1">
-              <label htmlFor="nomeUsuario">Nome</label>
-            <input 
-              type="text" 
-              id="nomeUsuario" 
+            <label htmlFor="nomeUsuario">Nome</label>
+            <input
+              type="text"
+              id="nomeUsuario"
               placeholder="user.name"
               className="border-2 border-blue-700 rounded-md px-2 py-1 hover:border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-300"
               {...register("nomeUsuario", {
                 required: "Campo nome é obrigatório",
-                minLength: {value: 3, message: "Mínimo 3 caracteres"},
+                minLength: { value: 3, message: "Mínimo 3 caracteres" },
               })}
             />
             {errors.nomeUsuario && (
               <small className="text-red-500">{errors.nomeUsuario.message}</small>
-             )}
+            )}
           </div>
           <div className="grid gap-y-1">
             <label htmlFor="email">E-mail</label>
-            <input 
-              type="email" 
-              id="email" 
+            <input
+              type="email"
+              id="email"
               placeholder="user.name@example.com"
               className="border-2 border-blue-700 rounded-md px-2 py-1 hover:border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-300"
-              {...register("email", { 
-                required: "Campo nome é obrigatório",
-                minLength: {value: 3, message: "Mínimo 3 caracteres"},
-            })}
+              {...register("email", {
+                required: "Campo email é obrigatório",
+                minLength: { value: 3, message: "Mínimo 3 caracteres" },
+              })}
             />
-             {errors.email && (
+            {errors.email && (
               <small className="text-red-500">{errors.email.message}</small>
-             )}
-            <small className="text-red-500">Erro</small>
+            )}
           </div>
 
           <button className="px-4 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-300 justify-self-end">Entrar</button>
@@ -64,4 +84,4 @@ function Login() {
   );
 }
 
-export {Login};
+export { Login };
